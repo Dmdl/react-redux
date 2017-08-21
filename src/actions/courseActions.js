a009bc2d9d3d55ffb14e0a1e8330e6b800559c8c
lakmal <lada@mazarin.lk>
2017-08-21 14:43:01 +0530

    added save course function

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -5,12 +5,31 @@ export function loadCoursessuccess(courses) {
   return {type: types.LOAD_COURSES_SUCCESS, courses};
 }
 
+export function updateCourseSuccess(course) {
+  return {type: types.CREATE_COURSES_SUCCESS, course};
+}
+
+export function createCourseSuccess(course) {
+  return {type: types.UPDATE_COURSES_SUCCESS, course};
+}
+
 export function loadCourses() {
   return function (dispatch) {
     return courseApi.getAllCourses().then(courses => {
       dispatch(loadCoursessuccess(courses));
     }).catch(error => {
       throw (error);
+    });
+  };
+}
+
+export function saveCourse(course) {
+  return function (dispatch, getState) {
+    return courseApi.saveCourse(course).then(course => {
+      course.id ? dispatch(updateCourseSuccess(course)) :
+        dispatch(createCourseSuccess(course));
+    }).catch(error => {
+      throw(error);
     });
   };
 }
